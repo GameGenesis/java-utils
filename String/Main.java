442a00e5309f5df7423b92ea8ab6efc490819c99
GameGenesis <gamegenesisdev@gmail.com>
2022-09-12 23:24:44 -0400

    Update Str example

--- String/Main.java
+++ String/Main.java
@@ -1,12 +1,14 @@
 package String;
 
 public class Main {
-    public static String first = "John Smith";
+    public static String name = "John Smith";
     public static int age = 24;
     public static float temperature = 100;
 
     public static void main(String[] args) {
-        System.out.println(StringFormat.fmt("My name is {0} and I am {1.2f} years old. I have {$1} or {1:C}", first, age));
-        System.out.println(StringFormat.fmt("The temperature is {0:tc} or {1:TF}", temperature, temperature * (9f/5f) + 32f));
+        Str.print(Str.fmt("My name is {0} and I am {1:2f} years old. I have {$1} or {1:C}", name, age));
+        Str.print(Str.fmt("The temperature is {0:tc} or {1:TF}", temperature, temperature * (9f/5f) + 32f));
+        Str.print("This", "is", "an", "example", "of", "the", "print", "method");
+        Str.print(", ".toCharArray(), ".".toCharArray(), "Element0", "Element1", "Element2");
     }
 }
