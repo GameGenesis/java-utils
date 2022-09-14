8b5dfad2ad7e4a8177c859f838f5c2d05468bcef
Rayan Kaissi <gamegenesisdev@gmail.com>
2022-09-14 09:08:18 -0400

    Create Str.isUpper method

--- String/Str.java
+++ String/Str.java
@@ -146,6 +146,25 @@ public class Str {
     }
 
     /**
+     * Returns true if all the characters in a string are lower case
+     * @param string The string to check
+     * @return Whether all the characters in a string are lower case
+     */
+    public static boolean isUpper(String string) {
+        char ch;
+        boolean isUpper = true;
+        for (int i = 0; i < string.length(); i++) {
+            ch = string.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                isUpper = false;
+                break;
+            }
+        };
+
+        return isUpper;
+    }
+
+    /**
      * Converts the value of objects to strings based on the formats specified and inserts them into another string.
      * Replaces the format item in a specified string with the string representation of a corresponding object in a specified array.
      * <p>
